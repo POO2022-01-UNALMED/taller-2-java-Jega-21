69613783fac706e15e96a537ea1887ce0b87f4d3
Jega-21 <jhgallegoa@unal.edu.co>
2022-03-30 01:20:24 -0500

    Taller 2
    Retroalimentación.

--- src/test/java/test/Asiento.java
+++ src/test/java/test/Asiento.java
@@ -5,12 +5,12 @@ public class Asiento {
 	public int registro;
 	
 	Asiento (String cambiarColor) {
-		if (color == "rojo" || cambiarColor == "verde" || cambiarColor == "amarillo" || cambiarColor == "negro" || cambiarColor == "blanco") {
+		if (cambiarColor == "rojo" || cambiarColor == "verde" || cambiarColor == "amarillo" || cambiarColor == "negro" || cambiarColor == "blanco") {
 			color = cambiarColor;
 		}
 	}
-	void cambiarColor(String color) {
-		this.color = color;
+	void cambiarColor(String cambiarColor) {
+		this.color = cambiarColor;
 	}
 	
 }	
